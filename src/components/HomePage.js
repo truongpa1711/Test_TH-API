3def1c7bcf1b0cc88fc038adf604846786fcd6fa
truongpa <truongvuu@gmail.com>
2023-07-27 17:08:57 +0700

    Quản lý nhân viên

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -23,7 +23,7 @@ function HomePage(props) {
     }
 
     const load = () => {
-        let url = "https://64bfca600d8e251fd1116f33.mockapi.io/truongpa";
+        let url = "https://64bfca600d8e251fd1116f33.mockapi.io/truongpa/nhanvien";
         axios.get(url)
             .then(res => { console.log(res.data) })
             .catch(err => { console.error(err) });
@@ -37,10 +37,9 @@ function HomePage(props) {
             axios.delete(url)
                 .then(res => {
                     console.log("Xóa thành công!");
-                    pb(); nv(); load();
-                    })
+                    pb(); nv();
+                })
                 .catch(err => console.error(err));
-            load();
         }
     }
     useEffect(() => {
@@ -53,45 +52,49 @@ function HomePage(props) {
         <div>
             <h3>Danh sách phòng ban</h3>
             <table border={1}>
-                <tr>
-                    <td>STT</td>
-                    <td>ID</td>
-                    <td>Tên</td>
-                </tr>
-                {phongban?.map((item, index) => (
-                    <tr key={index}>
-                        <td>{index + 1}</td>
-                        <td>{item.idpb}</td>
-                        <td>{item.ten}</td>
+                <tbody>
+                    <tr>
+                        <td>STT</td>
+                        <td>ID</td>
+                        <td>Tên</td>
                     </tr>
-                ))}
+                    {phongban?.map((item, index) => (
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{item.idpb}</td>
+                            <td>{item.ten}</td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
             <h3>Danh sách nhân viên</h3>
             <table border={1}>
-                <tr>
-                    <td>STT</td>
-                    <td>ID Nhân viên</td>
-                    <td>ID Phòng ban</td>
-                    <td>Tên</td>
-                    <td>SĐT</td>
-                    <td>Trạng thái</td>
-                    <td>Địa chỉ</td>
-                    <td>Update</td>
-                    <td>Delete</td>
-                </tr>
-                {nhanvien?.map((item, index) => (
-                    <tr key={index}>
-                        <td>{index + 1}</td>
-                        <td>{item.idnv}</td>
-                        <td>{item.idpbnv}</td>
-                        <td>{item.ten}</td>
-                        <td>{item.sdt}</td>
-                        <td>{item.trangthai ? "Đang làm" : "Đã nghỉ"}</td>
-                        <td>{item.diachi}</td>
-                        <td align='center'><Link to={"/form/" + item.id}><img src={imgUpdate} /></Link></td>
-                        <td align='center'><Link><img src={imgDelete} onClick={() => deleteNv(index)} /></Link></td>
+                <tbody>
+                    <tr>
+                        <td>STT</td>
+                        <td>ID Nhân viên</td>
+                        <td>ID Phòng ban</td>
+                        <td>Tên</td>
+                        <td>SĐT</td>
+                        <td>Trạng thái</td>
+                        <td>Địa chỉ</td>
+                        <td>Update</td>
+                        <td>Delete</td>
                     </tr>
-                ))}
+                    {nhanvien?.map((item, index) => (
+                        <tr key={index}>
+                            <td>{index + 1}</td>
+                            <td>{item.idnv}</td>
+                            <td>{item.idpbnv}</td>
+                            <td>{item.ten}</td>
+                            <td>{item.sdt}</td>
+                            <td>{item.trangthai ? "Đang làm" : "Đã nghỉ"}</td>
+                            <td>{item.diachi}</td>
+                            <td align='center'><Link to={"/form/" + item.id}><img src={imgUpdate} /></Link></td>
+                            <td align='center'><Link><img src={imgDelete} onClick={() => deleteNv(index)} /></Link></td>
+                        </tr>
+                    ))}
+                </tbody>    
             </table>
         </div>
     );
